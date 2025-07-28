9d95f0d32b01277eb2e5ed5339433fa098c1c2ba
Ritesh <ritesh@Riteshs-MacBook-Pro.local>
2025-07-28 20:29:37 +0530

    Added the nginx proxy and a bash script to toggle the blue and green deployment.

--- docker/nginx/promote.sh
+++ docker/nginx/promote.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-set -e
-
 TARGET=$1
 
 if [[ "$TARGET" != "blue" && "$TARGET" != "green" ]]; then
-  echo "Usage: $0 [blue|green]"
+  echo "[promote] Invalid target. Use: blue or green"
   exit 1
 fi
 
 echo "[promote] Switching to $TARGET"
 
-ln -sf /etc/nginx/sites-available/$TARGET.conf /etc/nginx/sites-enabled/default.conf
+# Render template
+sed "s/__TARGET__/app_$TARGET/" /etc/nginx/upstream.conf.template > /etc/nginx/conf.d/upstream.conf
+
+# Reload nginx config
 nginx -s reload
